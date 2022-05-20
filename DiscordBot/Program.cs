16cb55ae3b9fa109448bf3b6884ed856af72e16d
Charles Weiss <cweiss1995@gmail.com>
2022-05-20 22:56:08 +0800

    Starting to implement the interaction framework

--- DiscordBot/Program.cs
+++ DiscordBot/Program.cs
@@ -1,6 +1,8 @@
 ﻿using Discord;
+using Discord.Interactions;
 using Discord.Net;
 using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
 using Newtonsoft.Json;
 
 namespace DiscordBot
@@ -8,23 +10,34 @@ namespace DiscordBot
     public class Program
     {
         const string TokenFileEnvVar = "KingHarley_Bot_File_Path";
-        private DiscordSocketClient _client;
+        private readonly IServiceProvider _services;
+        private readonly DiscordSocketConfig _socketConfig = new()
+        {
+            GatewayIntents = GatewayIntents.AllUnprivileged | GatewayIntents.GuildMembers,
+            AlwaysDownloadUsers = true
+        };
 
         public Program()
         {
-            _client = new DiscordSocketClient();
+            _services = new ServiceCollection()
+                .AddSingleton(_socketConfig)
+                .AddSingleton<DiscordSocketClient>()
+                .AddSingleton(x => new InteractionService(x.GetRequiredService<DiscordSocketClient>()))
+                .AddSingleton<InteractionHandler>()
+                .BuildServiceProvider();
         }
 
-        public static Task Main(string[] args) => new Program().MainAsync();
-        public async Task MainAsync()
+        public static void Main() => new Program().RunAsync().GetAwaiter().GetResult();
+        public async Task RunAsync()
         {
+            var client = _services.GetRequiredService<DiscordSocketClient>();
+            client.Log += LogMessage;
+
+            await _services.GetRequiredService<InteractionHandler>().InitializeAsync();
+
             var token_path = Environment.GetEnvironmentVariable(TokenFileEnvVar);
             if (token_path == null)
                 throw new System.Exception($"The {TokenFileEnvVar} environment variable is not set. Make sure to set this environment variable to the absolute path of .json file containing the bot token");
-
-            _client.Log += LogMessage;
-            _client.Ready += ClientReady;
-            _client.SlashCommandExecuted += SlashCommandHandler;
 
             using var r = new StreamReader(token_path);
 
@@ -35,29 +48,11 @@ namespace DiscordBot
             if (!token_dict.TryGetValue("token", out var token))
                 throw new System.Exception("The token is missing");
 
-            await _client.LoginAsync(TokenType.Bot, token);
-            await _client.StartAsync();
+            await client.LoginAsync(TokenType.Bot, token);
+            await client.StartAsync();
 
             await Task.Delay(-1);
 
-        }
-
-        private async Task ClientReady()
-        {
-            var slashCommand = new SlashCommandBuilder().WithName("First-Command").WithDescription("This is the first command");
-
-            try
-            {
-                await _client.CreateGlobalApplicationCommandAsync(slashCommand.Build());
-            }
-            catch (HttpException ex)
-            {
-                Console.WriteLine(ex.Reason);
-            }
-        }
-        private async Task SlashCommandHandler(SocketSlashCommand command)
-        {
-            await command.RespondAsync(command.CommandName);
         }
 
         private Task LogMessage(LogMessage message)
@@ -66,6 +61,14 @@ namespace DiscordBot
             return Task.CompletedTask;
         }
 
+        public static bool IsDebug()
+        {
+#if DEBUG
+            return true;
+#else
+                return false;
+#endif
+        }
     }
 }
 
